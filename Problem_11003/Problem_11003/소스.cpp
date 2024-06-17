cd48742b0ec131bf132ef5f4c361e33bbd1b921c
Jekovic <jjwan0203@naver.com>
2024-06-17 13:55:28 +0900

    11003 update

--- Problem_11003/Problem_11003/소스.cpp
+++ Problem_11003/Problem_11003/소스.cpp
@@ -1,3 +1,66 @@
+//#include <iostream>
+//#include <queue>
+//#include <vector>
+//#include <algorithm>
+//#include <stack>
+//#include <cmath>
+//#include <cstring>
+//
+//using namespace std;
+//
+//int N, L;
+//vector<int> num;
+//
+//struct A {
+//	int val;
+//	int idx;
+//};
+//
+//struct cmp {
+//	bool operator()(const A& a, const A& b) {
+//		return a.val > b.val;	
+//	}
+//};
+//
+//void input() {
+//	cin >> N >> L;
+//
+//	num.assign(N, 0);
+//
+//	for (int i = 0; i < N; i++) {
+//		cin >> num[i];
+//	}
+//}
+//
+//void solve() {
+//	priority_queue<A, vector<A>, cmp> pq;
+//	
+//	for (int i = 0; i < N; i++) {
+//		A temp;
+//		temp.val = num[i];
+//		temp.idx = i;
+//		pq.push(temp);
+//		int pos = pq.top().idx;
+//		while (pos < i - L + 1) {
+//			pq.pop();
+//			pos = pq.top().idx;
+//		}
+//		cout << pq.top().val << ' ';
+//	}
+//}
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	input();
+//
+//	solve();
+//
+//	return 0;
+//}
+
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -18,7 +81,7 @@ struct A {
 
 struct cmp {
 	bool operator()(const A& a, const A& b) {
-		return a.val > b.val;	
+		return a.val > b.val;
 	}
 };
 
@@ -33,19 +96,17 @@ void input() {
 }
 
 void solve() {
-	priority_queue<A, vector<A>, cmp> pq;
-	
+	deque<A> q;
+
 	for (int i = 0; i < N; i++) {
-		A temp;
-		temp.val = num[i];
-		temp.idx = i;
-		pq.push(temp);
-		int pos = pq.top().idx;
-		while (pos < i - L + 1) {
-			pq.pop();
-			pos = pq.top().idx;
-		}
-		cout << pq.top().val << ' ';
+		if (!q.empty() and i == q.front().idx + L)
+			q.pop_front();
+
+		while (!q.empty() and q.back().val > num[i])
+			q.pop_back();
+
+		q.push_back({ num[i], i });
+		cout << q.front().val << ' ';
 	}
 }
 
